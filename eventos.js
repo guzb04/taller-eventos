b3bdad5fb3dd00c657c3fb0cd3422ed13a6e5704
Guzb04 <guzmanbeyhaut@gmail.com>
2023-08-13 16:47:53 -0400

    tercer push

---
+++ eventos.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.getElementById("eldiv").addEventListener("click", function(){
+
+        alert("hola, soy el div")
+        console.log("clicked")
+        
+        })
+
+})
+
+
+
+
